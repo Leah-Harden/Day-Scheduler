04b410a0ff76345afdb4deffd2fcb420b10089df
Leah <leahxharden@gmail.com>
2023-01-31 22:30:15 -0500

    finishing the Javascript

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ $(document).ready(function () {
 
   var text8 = $('#8 textarea')
   var text9 = $('#9 textarea')
+  var text10 = $('#10 textarea')
+  var text11 = $('#11 textarea')
+  var text12 = $('#12 textarea')
+  var text13 = $('#13 textarea')
+  var text14 = $('#14 textarea')
+  var text15 = $('#15 textarea')
+  var text16 = $('#16 textarea')
+  var text17 = $('#17 textarea')
+  var text18 = $('#18 textarea')
+  var text19 = $('#19 textarea')
+  var text20 = $('#20 textarea')
+  var text21 = $('#21 textarea')
+
 
   //time part
   var now = dayjs().format('MMMM-DD-dddd')
@@ -47,6 +60,18 @@ $(document).ready(function () {
   if (list) {
     text8.text(list['8'])
     text9.text(list['9'])
+    text10.text(list['10'])
+    text11.text(list['11'])
+    text12.text(list['12'])
+    text13.text(list['13'])
+    text14.text(list['14'])
+    text15.text(list['15'])
+    text16.text(list['16'])
+    text17.text(list['17'])
+    text18.text(list['18'])
+    text19.text(list['19'])
+    text20.text(list['20'])
+    text21.text(list['21'])
   }
   // localStorage.setItem(id, value);
   // var x = id;
